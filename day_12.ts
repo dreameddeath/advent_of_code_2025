08088da8c224a8bb4d67b3b7ba1541773b5dc5ab
christophe jeunesse <christophe.jeunesse@orange.com>
2025-12-25 18:27:35 +0100

    Day 12 (cheat?)

--- day_12.ts
+++ day_12.ts
@@ -1,17 +1,86 @@
 import { Logger, Part, run, Type } from "./day_utils"
 
-function parse(lines: string[]): string[] {
-    return lines;
+type ShapePart = "#" | "."
+type ShapeLine = [ShapePart, ShapePart, ShapePart];
+interface Shape {
+    id: number,
+    parts: ShapeLine[],
+    nbEmpty: number,
+}
+
+interface Region {
+    lineId: number;
+    width: number;
+    height: number;
+    constraints: number[];
+}
+
+interface World {
+    shapes: Shape[],
+    regions: Region[]
+}
+const START_SHAPE = /^\d+:\s*$/;
+const PART_SHAPE = /^[#\.]{3}$/;
+const REGION = /^(\d+)x(\d+):\s+(.+)*?\s*$/;
+function parse(lines: string[]): World {
+    const shapes: Shape[] = [];
+    let currShape: Shape | undefined;
+    const regions: Region[] = [];
+    lines.forEach((l, pos) => {
+        const regionMatch = REGION.exec(l);
+
+        if (l.trim() === "") {
+            currShape = undefined;
+            return;
+        }
+        else if (PART_SHAPE.test(l)) {
+            const shapeLine = l.split("") as ShapeLine;
+            currShape!.parts.push(shapeLine);
+            currShape!.nbEmpty += shapeLine.filter(p => p === ".").length;
+        }
+        else if (START_SHAPE.test(l)) {
+            currShape = {
+                id: shapes.length,
+                parts: [],
+                nbEmpty: 0
+            };
+            shapes.push(currShape);
+            return;
+        }
+        else if (regionMatch) {
+            regions.push({
+                lineId: pos + 1,
+                width: parseInt(regionMatch[1], 10),
+                height: parseInt(regionMatch[2], 10),
+                constraints: regionMatch[3].split(/\s+/).map(c => parseInt(c, 10))
+            })
+        }
+
+    })
+    return {
+        shapes,
+        regions
+    };
 }
 
 function puzzle(lines: string[], part: Part, type: Type, logger: Logger): void {
     const data = parse(lines);
     if (part === Part.PART_1) {
-        const result = data.length;
-        logger.result(result, [undefined, undefined])
+        const shapeAvgDensity = Math.round(data.shapes.map(s => (9 - s.nbEmpty) / 9).reduce((a, b) => a + b) * 100 / data.shapes.length);
+        let count = 0;
+        for (const region of data.regions) {
+            const countCells = region.height * region.width;
+            const requestedCell = region.constraints.map((qty, pos) => (9 - data.shapes[pos].nbEmpty) * qty).reduce((a, b) => a + b);
+            const density = Math.round((requestedCell * 1.0 / countCells) * 100)
+            if (density < 88) {
+                count++;
+            }
+        }
+        const result = data.shapes.length;
+        logger.result(count, [2, 422])
     }
     else {
-        const result = data.length;
+        const result = data.shapes.length;
         logger.result(result, [undefined, undefined])
     }
 }
@@ -21,4 +90,4 @@ function puzzle(lines: string[], part: Part, type: Type, logger: Logger): void {
  * Adapt types list to your needs and parts also 
  * @see run javadoc
  */
-run(99, [Type.TEST, Type.RUN], puzzle, [Part.PART_1, Part.PART_2])
+run(12, [Type.TEST, Type.RUN], puzzle, [Part.PART_1, Part.PART_2])
